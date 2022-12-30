0e985f61741033af7abc1476af379e0b540e9526
Jared Mullins <jwmullins92@gmail.com>
2022-12-29 20:22:18 -0600

    test

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import UserMap from './UserMap'
+
+export default function Home(props) {
+    return (
+        <UserMap user={props.user} allUsers={props.allUsers} />
+    )
+
+}
